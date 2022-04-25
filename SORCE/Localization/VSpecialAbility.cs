5da40869c45eb8c8d749f7e2aea06c14facf1263
FN LN <lucas87t@gmail.com>
2022-04-25 10:04:27 -0500

    Attempted: NotSwearwolves

---
+++ SORCE/Localization/VSpecialAbility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SORCE.Localization
+{
+	public static class VSpecialAbility // Vanilla Special Abilities
+	{
+		public const string
+				Bite = "Bite",
+				Camouflage = "Camouflage",
+				Cannibalize = "Cannibalize",
+				Chaaarge = "Charge",
+				ChloroformHankie = "ChloroformHankie",
+				CryProfusely = "TutorialAbility",
+				Enslave = "Enslave",
+				Handcuffs = "Handcuffs",
+				Joke = "Joke",
+				Laptop = "Laptop",
+				LaserGun = "LaserGun",
+				MechTransformation = "MechTransform",
+				MindControl = "MindControl",
+				Possess = "Possess",
+				PowerSap = "PowerSap",
+				PrimalLunge = "Lunge",
+				SharpLunge = "WerewolfLunge",
+				StickyGlove = "StealingGlove",
+				Stomp = "Stomp",
+				Toss = "Toss",
+				WaterCannon = "WaterCannon",
+				WerewolfTransformation = "WerewolfTransform",
+				ZombieSpit = "ZombieSpit";
+	}
+
+}
