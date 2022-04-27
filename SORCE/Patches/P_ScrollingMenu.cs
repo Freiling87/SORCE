d16ecb393208a2b8ef9300505c14c9b18c742192
FN LN <lucas87t@gmail.com>
2022-04-27 10:32:31 -0500

    Attempted Ambient Light Color

--- SORCE/Patches/P_ScrollingMenu.cs
+++ SORCE/Patches/P_ScrollingMenu.cs
@@ -1,19 +1,39 @@
-﻿using HarmonyLib;
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using RogueLibsCore;
+using SORCE.Challenges.C_AmbientLightColor;
 using SORCE.Challenges.C_AmbientLightLevel;
 using SORCE.Challenges.C_Lighting;
 using SORCE.Challenges.C_VFX;
+using SORCE.Logging;
 using SORCE.Patches.P_PlayfieldObject;
+using System.Linq;
+using UnityEngine;
 
 namespace SORCE.Patches.Interface
 {
     [HarmonyPatch(declaringType: typeof(ScrollingMenu))]
 	class P_ScrollingMenu
 	{
+		private static readonly ManualLogSource logger = SORCELogger.GetLogger();
 		public static GameController GC => GameController.gameController;
 
 		[HarmonyPostfix, HarmonyPatch(methodName: nameof(ScrollingMenu.PushedButton), argumentTypes: new[] { typeof(ButtonHelper) })]
 		public static void PushedButton_Postfix(ButtonHelper myButton, ScrollingMenu __instance)
 		{
+			logger.LogDebug("Button Name:\t" + myButton.name);
+			logger.LogDebug("Button Text:\t:" + myButton.myText);
+
+			// TODO: Try doing all of this in LevelTransition.ChangeLevel 
+			if (RogueFramework.Unlocks.OfType<AmbientLightColorChallenge>().Any(c => c.Name == myButton.name))
+            {
+				Color32 color = RogueFramework.Unlocks.OfType<AmbientLightColorChallenge>().FirstOrDefault(c => c.IsEnabled).FilterColor;
+
+				GC.levelFeelingsScript.floorCover.color = color;
+				RenderSettings.ambientSkyColor = color;
+				RenderSettings.ambientIntensity = 255;
+			}
+
 			// TODO: Eliminate this when you find a more elegant fix
 			if (GC.challenges.Contains(nameof(NewMoon)))
 				GC.cameraScript.lightingSystem.EnableAmbientLight = false;
