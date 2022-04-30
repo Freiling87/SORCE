86bc5771efb88299b943972bf3b7412d40a35a19
Ted Bunny <lucas87t@gmail.com>
2022-04-30 13:21:49 -0500

    Water smoke gate

--- SORCE/Patches/P_PlayfieldObject/P_Water.cs
+++ SORCE/Patches/P_PlayfieldObject/P_Water.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx.Logging;
 using HarmonyLib;
+using SORCE.Challenges.C_Features;
 using SORCE.Logging;
 using UnityEngine;
 using static SORCE.Localization.NameLists;
@@ -12,12 +13,20 @@ namespace SORCE.Patches.P_PlayfieldObject
         private static readonly ManualLogSource logger = SORCELogger.GetLogger();
         public static GameController GC => GameController.gameController;
 
+        /// <summary>
+        /// ThePollutionSolution Poisoned Water Smoke particles
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="doClean"></param>
+        /// <param name="myCauserAgent"></param>
+        /// <param name="__instance"></param>
         [HarmonyPostfix, HarmonyPatch(methodName: nameof(Water.SpreadPoisonStart), argumentTypes: new[] { typeof(InvItem), typeof(bool), typeof(Agent) })]
         public static void SpreadPoisonStart_Postfix(InvItem item, bool doClean, Agent myCauserAgent, Water __instance)
         {
-            foreach (Vector2 tile in __instance.waterTiles)
-                if (GC.percentChance(2))
-                    GC.spawnerMain.SpawnParticleEffect(VParticleEffect.SmokePuffs, new Vector3(tile.x * 0.64f, tile.y * 0.64f, __instance.tr.position.z), 0f);
+            if (GC.challenges.Contains(nameof(ThePollutionSolution)))
+                foreach (Vector2 tile in __instance.waterTiles)
+                    if (GC.percentChance(2))
+                        GC.spawnerMain.SpawnParticleEffect(VParticleEffect.SmokePuffs, new Vector3(tile.x * 0.64f, tile.y * 0.64f, __instance.tr.position.z), 0f);
         }
     }
 }
